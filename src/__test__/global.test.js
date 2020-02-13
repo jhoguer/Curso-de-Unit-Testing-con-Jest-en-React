64133ca0d7d70f42ccb8f07705a713b8836c1196
Jhon Guerrero <jhoguer@gmail.com>
2020-02-13 14:03:55 -0500

    Clonando repositorio de platziStore

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -44,3 +44,14 @@ test('Probar una promesa', () => {
       expect(string).toBe('aloH');
     });
 });
+
+test('Probar async/await', async () => {
+  const str = await revertString2('Hola')
+  expect(str).toBe('aloH');
+})
+
+// afterEach(() => console.log('Despues de cada prueba'));
+// afterAll(() => console.log('Despues de todas las pruebas'));
+
+// beforeEach(() => console.log('Antes de cada prueba'));
+// beforeAll(() => console.log('Antes de toas las pruebas'));
